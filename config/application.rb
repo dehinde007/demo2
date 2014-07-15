11e0f3b158aee64002426263046d3744a40ad800
dehinde007 <dehinde007@gmail.com>
2014-07-15 22:01:42 +0100

    lol

--- config/application.rb
+++ config/application.rb
@@ -27,6 +27,5 @@
         I18n.enforce_available_locales = true
         config.serve_static_assets = true
         config.assets.precompile += %w(*.png *.jpg *.jpeg *.gif)
-        config.assets.initialize_on_precompile = true
       end
     end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,14 +19,14 @@ SampleApp::Application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  config.serve_static_assets = true
+  config.serve_static_assets = false
 
   # Compress JavaScripts and CSS.
   config.assets.js_compressor  = :uglifier
   # config.assets.css_compressor = :sass
 
   # Whether to fallback to assets pipeline if a precompiled asset is missed.
-  config.assets.compile = true
+  config.assets.compile = false
 
   # Generate digests for assets URLs.
   config.assets.digest = true
