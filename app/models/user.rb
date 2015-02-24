322f06701f27b8762e8480bd6879c551f202df05
dehinde007 <dehinde007@gmail.com>
2015-02-24 02:14:10 +0100

    news

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,7 +3,7 @@ class User < ActiveRecord::Base
 #user search
   def self.search(search)
     if search 
-      where('name LIKE ?', "%#{search}%")
+      where('name ILIKE ?', "%#{search}%")
     else
    scoped  
     end
