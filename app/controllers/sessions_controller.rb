872674345957909b9c2d73111f1a993f0ef68b53
dehinde007 <dehinde007@gmail.com>
2015-11-27 19:36:30 +0100

    Revert "lol"
    This reverts commit 7671b9acae4f62d28cbe506a792a9cf18f1fc78d.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,8 @@ class SessionsController < ApplicationController
     
   end
 
-  def create    
+  def create
+    
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
@@ -19,5 +20,4 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_url
   end
-  
 end
