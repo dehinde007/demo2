545ad094ba75c6025493e76f3187928a47e09b03
dehinde007 <dehinde007@gmail.com>
2015-11-28 14:00:29 +0100

    Revert "shit"
    This reverts commit e72b04526f11547b6be87f833e7e6cbc4d307b59.

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,6 +1,14 @@
 class FeedsController < ApplicationController
   before_action :signed_in_user
 
+
+  def home
+    if signed_in?
+      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 10)   
+    else 
+      redirect_to signin_path
+    end
+  end
   
   def feed
     
--- app/controllers/invitations_controller.rb
+++ app/controllers/invitations_controller.rb
@@ -14,7 +14,7 @@ class InvitationsController < ApplicationController
       redirect_to root_url
     else
       flash[:notice] = "Thank you, we will notify when we are ready."
-      render "new"
+      redirect_to 
     end
  end
  
