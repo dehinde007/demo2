e72b04526f11547b6be87f833e7e6cbc4d307b59
dehinde007 <dehinde007@gmail.com>
2015-11-28 13:59:57 +0100

    shit

--- app/controllers/feeds_controller.rb
+++ app/controllers/feeds_controller.rb
@@ -1,14 +1,6 @@
 class FeedsController < ApplicationController
   before_action :signed_in_user
 
-
-  def home
-    if signed_in?
-      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 10)   
-    else 
-      redirect_to signin_path
-    end
-  end
   
   def feed
     
