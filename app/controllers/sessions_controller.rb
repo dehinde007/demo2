5c1c0660cd0591f2f6aed3c3935660792490101b
dehinde007 <dehinde007@gmail.com>
2015-11-28 16:16:37 +0100

    Revert "jjj"
    This reverts commit bacbc23be91a3ac5f129c52695d13bac6d9536fd.

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,11 +4,12 @@ class SessionsController < ApplicationController
     
   end
 
-  def create   
+  def create
+    
     user = User.find_by(email: params[:session][:email].downcase)
     if user && user.authenticate(params[:session][:password])
       sign_in user
-      redirect_to home_path
+      redirect_back_or home_path
     else
       flash[:alert] = 'Invalid email/password combination'
       render 'new'
@@ -19,6 +20,4 @@ class SessionsController < ApplicationController
     sign_out
     redirect_to root_url
   end
-  
-  
 end
--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -8,14 +8,6 @@ class StaticPagesController < ApplicationController
     end
   end
   
-   def home
-    if signed_in?
-      @feed_items = current_user.feed.paginate(page: params[:page], :per_page => 10)   
-    else 
-      redirect_to signin_path
-    end
-  end
-  
   def help
     
   end
