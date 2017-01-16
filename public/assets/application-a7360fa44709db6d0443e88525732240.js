c0658bf5d58fe932802cb07b4c8d042e16cc0738
Dehinde Molade <dehinde007@gmail.com>
2017-01-16 09:56:50 +0100

    cloudcdn-up

--- public/assets/application-a7360fa44709db6d0443e88525732240.js
+++ public/assets/application-a7360fa44709db6d0443e88525732240.js
@@ -36290,158 +36290,52 @@ return /******/ (function(modules) { // webpackBootstrap
 
 
 
+$(function()  {
+$(document).on('ready page:load', function ()  {
 
+/* global ImglyKit */
+"use strict";
 
+  /*
+   * Initialize ImglyKit
+   */
+  var kit = new ImglyKit({
+    renderer: "canvas", // Defaults to "webgl", uses "canvas" as fallback
+    assetsUrl: "/imglykit/assets", // The URL / path where all assets are
+    container: document.querySelector("#container"),
+    ui: {
+      enabled: true,
+      showExportButton: true,
+      export: {
+        type: ImglyKit.ImageFormat.JPEG
+      }
+    },
+    renderOnWindowResize: true // Our editor's size is relative to the window size
+  });
 
-  //Adslides  
+  kit.run();
 
-$(document).on('ready page:load', function () {
-    $('.fadein p:gt(0)').hide();
-    setInterval(function(){$('.fadein > :first-child').fadeOut().next('p').fadeIn("slow").end().appendTo('.fadein');}, 4000);
+  /*
+   * We have a "Render" button which (on click) will request the rendered
+   * image from ImglyKit and add it to the DOM
+   */
+  var button = document.getElementById("render-button");
+  button.addEventListener("click", function (e) {
+    e.preventDefault();
+
+    // This will render the image with 100 pixels in width while
+    // respecting the aspect ratio
+    // Possible render types: image, data-url
+    var image = kit.render("image", "image/png")
+      .then(function (image) {
+        document.body.appendChild(image);
+      });
+  });
+  
+
+});
 });
 
-$(document).on('ready page:load', function () {
-    //rotation speed and timer
-    var speed = 9000;
-    var run = setInterval('rotate()', speed);    
-    
-    //grab the width and calculate left value
-    var item_width = $('#slides li').outerWidth(); 
-    var left_value = item_width * (-1); 
-        
-    //move the last item before first item, just in case user click prev button
-    $('#slides li:first').before($('#slides li:last'));
-    
-    //set the default item to the correct position 
-    $('#slides ul').css({'left' : left_value});
-    //if user clicked on prev button
-    $('#prev').click(function() {
-        //get the right position            
-        var left_indent = parseInt($('#slides ul').css('left')) + item_width;
-        //slide the item            
-        $('#slides ul').animate({'left' : left_indent}, 200,function(){    
-            //move the last item and put it as first item                
-            $('#slides li:first').before($('#slides li:last'));           
-            //set the default item to correct position
-            $('#slides ul').css({'left' : left_value});
-        
-        });
-        //cancel the link behavior            
-        return false;
-            
-    });
-    //if user clicked on next button
-    $('#next').click(function() {
-        
-        //get the right position
-        var left_indent = parseInt($('#slides ul').css('left')) - item_width;
-        
-        //slide the item
-        $('#slides ul').animate({'left' : left_indent}, 200, function () {
-            
-            //move the first item and put it as last item
-            $('#slides li:last').after($('#slides li:first'));                     
-            
-            //set the default item to correct position
-            $('#slides ul').css({'left' : left_value});
-        
-        });
-                 
-        //cancel the link behavior
-        return false;
-        
-    });        
-    
-    //if mouse hover, pause the auto rotation, otherwise rotate it
-    $('#slides').hover(
-        
-        function() {
-            clearInterval(run);
-        }, 
-        function() {
-            run = setInterval('rotate()', speed);    
-        }
-    ); 
-        
-});
-//a simple function to click next link
-//a timer will call this function, and the rotation will begin :)  
-function rotate() {
-    $('#next').click();
-}
-
-
-
-
-$(document).on('ready page:load', function () {
-    //rotation speed and timer
-    var speed = 5000;
-    var run = setInterval('rotate()', speed);    
-    
-    //grab the width and calculate left value
-    var item_width = $('#sliders li').outerWidth(); 
-    var left_value = item_width * (-1); 
-        
-    //move the last item before first item, just in case user click prev button
-    $('#sliders li:first').before($('#sliders li:last'));
-    
-    //set the default item to the correct position 
-    $('#sliders ul').css({'left' : left_value});
-    //if user clicked on prev button
-    $('#prev').click(function() {
-        //get the right position            
-        var left_indent = parseInt($('#sliders ul').css('left')) + item_width;
-        //slide the item            
-        $('#sliders ul').animate({'left' : left_indent}, 200,function(){    
-            //move the last item and put it as first item                
-            $('#sliders li:first').before($('#sliders li:last'));           
-            //set the default item to correct position
-            $('#sliders ul').css({'left' : left_value});
-        
-        });
-        //cancel the link behavior            
-        return false;
-            
-    });
-    //if user clicked on next button
-    $('#next').click(function() {
-        
-        //get the right position
-        var left_indent = parseInt($('#sliders ul').css('left')) - item_width;
-        
-        //slide the item
-        $('#sliders ul').animate({'left' : left_indent}, 200, function () {
-            
-            //move the first item and put it as last item
-            $('#sliders li:last').after($('#sliders li:first'));                     
-            
-            //set the default item to correct position
-            $('#sliders ul').css({'left' : left_value});
-        
-        });
-                 
-        //cancel the link behavior
-        return false;
-        
-    });        
-    
-    //if mouse hover, pause the auto rotation, otherwise rotate it
-    $('#sliders').hover(
-        
-        function() {
-            clearInterval(run);
-        }, 
-        function() {
-            run = setInterval('rotate()', speed);    
-        }
-    ); 
-        
-});
-//a simple function to click next link
-//a timer will call this function, and the rotation will begin :)  
-function rotate() {
-    $('#next').click();
-}
 
 
 
